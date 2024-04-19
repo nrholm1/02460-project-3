bbc3b2a72e44158eab792b3adb06f5636b667361
Niels Raunkjær Holm <nielsraunkjaer@gmail.com>
2024-04-20 00:05:42 +0200

    add a comma

--- src/utils.py
+++ src/utils.py
@@ -9,9 +9,9 @@ def get_mutag_dataset(device:str='cpu'):
 def plot_adj(adj: Tensor, ax=None, name="Adjacency Matrix"):
     if ax is None:
         plt.spy(adj)
-        plt.title(f"{name} shape=[{adj.shape[0]}x{adj.shape[1]}]")
+        plt.title(f"{name}, shape=[{adj.shape[0]}x{adj.shape[1]}]")
         plt.tight_layout()
     else:
         ax.spy(adj)
-        ax.set_title(f"{name} shape=[{adj.shape[0]}x{adj.shape[1]}]")
+        ax.set_title(f"{name}, shape=[{adj.shape[0]}x{adj.shape[1]}]")
         ax.grid(False)
