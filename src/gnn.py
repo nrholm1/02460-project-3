e8372778c41affefc9aa5c804c665fa9e38140d6
Niels Raunkjær Holm <nielsraunkjaer@gmail.com>
2024-04-25 09:25:53 +0200

    Create files for deep generative models

---
+++ src/gnn.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class MessagePassingNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        ...
+
+
+class GraphConvNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        ...
+
